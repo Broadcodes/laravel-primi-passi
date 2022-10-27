c21cbd3071671ccbb6d90b4085611de264574bee
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-10-27 14:52:41 +0200

    Visualizzazione dinamica dei dati presenti in php

--- laravel-primi-passi/resources/views/home.blade.php
+++ laravel-primi-passi/resources/views/home.blade.php
@@ -1,1 +1,21 @@
 <h1>Hello World</h1>
+
+@php
+    $dataAuthorProject = [
+        'Name' => 'Gianluca',
+        'Surname' => 'Fabrizio',
+        'Name GitHub' => 'Broadcodes',
+        'Name Project' => 'laravel-primi-passi',
+        'Version Laravel' => '7',
+        'Course' => 'Boolean',
+    ];
+@endphp
+
+<h2>Visualizzazione dinamica dei dati presenti in php:</h2>
+<ul>
+    @foreach ($dataAuthorProject as $key => $value)
+        <li>
+            <h3>{{ $key }}: {{ $value }}</h3>
+        </li>
+    @endforeach
+</ul>
